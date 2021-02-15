07abd4f14733abe8dffafe4f147e12be0c238686
Tom Pickard <tom@tompickard.com>
2021-02-15 16:49:52 +0000

    Update to latest version of fastcast

--- src/TypeDefinitions.lua
+++ src/TypeDefinitions.lua
@@ -1,4 +1,6 @@
---!strict
+--!nocheck
+-- ^ change to strict to crash studio c:
+
 -- Defines all FC types.
 -- Any script that requires this will have these types defined.
 
@@ -22,66 +24,66 @@ export type GenericTable = {[any]: any}
 
 -- Represents a Caster :: https://etithespirit.github.io/FastCastAPIDocs/fastcast-objects/caster/
 export type Caster = {
-	WorldRoot: WorldRoot,
-	LengthChanged: RBXScriptSignal,
-	RayHit: RBXScriptSignal,
-	RayPierced: RBXScriptSignal,
-	CastTerminating: RBXScriptSignal,
-	Fire: (Vector3, Vector3, Vector3 | number, FastCastBehavior) -> ()
+  WorldRoot: WorldRoot,
+  LengthChanged: RBXScriptSignal,
+  RayHit: RBXScriptSignal,
+  RayPierced: RBXScriptSignal,
+  CastTerminating: RBXScriptSignal,
+  Fire: (Vector3, Vector3, Vector3 | number, FastCastBehavior) -> ()
 }
 
 -- Represents a FastCastBehavior :: https://etithespirit.github.io/FastCastAPIDocs/fastcast-objects/fcbehavior/
 export type FastCastBehavior = {
-	RaycastParams: RaycastParams?,
-	MaxDistance: number,
-	Acceleration: Vector3,
-	HighFidelityBehavior: number,
-	HighFidelitySegmentSize: number,
-	CosmeticBulletTemplate: Instance?,
-	CosmeticBulletProvider: any, -- Intended to be a PartCache. Dictated via TypeMarshaller.
-	CosmeticBulletContainer: Instance?,
-	AutoIgnoreContainer: boolean,
-	CanPierceFunction: CanPierceFunction
+  RaycastParams: RaycastParams?,
+  MaxDistance: number,
+  Acceleration: Vector3,
+  HighFidelityBehavior: number,
+  HighFidelitySegmentSize: number,
+  CosmeticBulletTemplate: Instance?,
+  CosmeticBulletProvider: any, -- Intended to be a PartCache. Dictated via TypeMarshaller.
+  CosmeticBulletContainer: Instance?,
+  AutoIgnoreContainer: boolean,
+  CanPierceFunction: CanPierceFunction
 }
 
 -- Represents a CastTrajectory :: https://etithespirit.github.io/FastCastAPIDocs/fastcast-objects/casttrajectory/
 export type CastTrajectory = {
-	StartTime: number,
-	EndTime: number,
-	Origin: Vector3,
-	InitialVelocity: Vector3,
-	Acceleration: Vector3
+  StartTime: number,
+  EndTime: number,
+  Origin: Vector3,
+  InitialVelocity: Vector3,
+  Acceleration: Vector3
 }
 
 -- Represents a CastStateInfo :: https://etithespirit.github.io/FastCastAPIDocs/fastcast-objects/caststateinfo/
 export type CastStateInfo = {
-	UpdateConnection: RBXScriptSignal,
-	HighFidelityBehavior: number,
-	HighFidelitySegmentSize: number,
-	Paused: boolean,
-	TotalRuntime: number,
-	DistanceCovered: number,
-	IsActivelySimulatingPierce: boolean,
-	IsActivelyResimulating: boolean,
-	CancelHighResCast: boolean,
-	Trajectories: {[number]: CastTrajectory}
+  UpdateConnection: RBXScriptSignal,
+  HighFidelityBehavior: number,
+  HighFidelitySegmentSize: number,
+  Paused: boolean,
+  TotalRuntime: number,
+  DistanceCovered: number,
+  IsActivelySimulatingPierce: boolean,
+  IsActivelyResimulating: boolean,
+  CancelHighResCast: boolean,
+  Trajectories: {[number]: CastTrajectory}
 }
 
 -- Represents a CastRayInfo :: https://etithespirit.github.io/FastCastAPIDocs/fastcast-objects/castrayinfo/
 export type CastRayInfo = {
-	Parameters: RaycastParams,
-	WorldRoot: WorldRoot,
-	MaxDistance: number,
-	CosmeticBulletObject: Instance?,
-	CanPierceCallback: CanPierceFunction
+  Parameters: RaycastParams,
+  WorldRoot: WorldRoot,
+  MaxDistance: number,
+  CosmeticBulletObject: Instance?,
+  CanPierceCallback: CanPierceFunction
 }
 
 -- Represents an ActiveCast :: https://etithespirit.github.io/FastCastAPIDocs/fastcast-objects/activecast/
 export type ActiveCast = {
-	Caster: Caster,
-	StateInfo: CastStateInfo,
-	RayInfo: CastRayInfo,
-	UserData: {[any]: any}
+  Caster: Caster,
+  StateInfo: CastStateInfo,
+  RayInfo: CastRayInfo,
+  UserData: {[any]: any}
 }
 
 return {}
--- src/init.lua
+++ src/init.lua
@@ -1,45 +1,45 @@
---!strict
--- on GOD if some of you report type mismatches and other stuff caused by issues in the luau type checking ***beta*** ima have to smack some of yall 
+--!nocheck
+-- ^ change to strict to crash studio c:
 
 --[[
-	Written by Eti the Spirit (18406183)
-	
-		The latest patch notes can be located here (and do note, the version at the top of this script might be outdated. I have a thing for forgetting to change it):
-		>	https://etithespirit.github.io/FastCastAPIDocs/changelog
-		
-		*** If anything is broken, please don't hesitate to message me! ***
-		
-		YOU CAN FIND IMPORTANT USAGE INFORMATION HERE: https://etithespirit.github.io/FastCastAPIDocs
-		YOU CAN FIND IMPORTANT USAGE INFORMATION HERE: https://etithespirit.github.io/FastCastAPIDocs
-		YOU CAN FIND IMPORTANT USAGE INFORMATION HERE: https://etithespirit.github.io/FastCastAPIDocs
-		
-		YOU SHOULD ONLY CREATE ONE CASTER PER GUN.
-		YOU SHOULD >>>NEVER<<< CREATE A NEW CASTER EVERY TIME THE GUN NEEDS TO BE FIRED.
-		
-		A caster (created with FastCast.new()) represents a "gun".
-		When you consider a gun, you think of stats like accuracy, bullet speed, etc. This is the info a caster stores. 
-	
-	--
-	
-	This is a library used to create hitscan-based guns that simulate projectile physics.
-	
-	This means:
-		- You don't have to worry about bullet lag / jittering
-		- You don't have to worry about keeping bullets at a low speed due to physics being finnicky between clients
-		- You don't have to worry about misfires in bullet's Touched event (e.g. where it may going so fast that it doesn't register)
-		
-	Hitscan-based guns are commonly seen in the form of laser beams, among other things. Hitscan simply raycasts out to a target
-	and says whether it hit or not.
-	
-	Unfortunately, while reliable in terms of saying if something got hit or not, this method alone cannot be used if you wish
-	to implement bullet travel time into a weapon. As a result of that, I made this library - an excellent remedy to this dilemma.
-	
-	FastCast is intended to be require()'d once in a script, as you can create as many casters as you need with FastCast.new()
-	This is generally handy since you can store settings and information in these casters, and even send them out to other scripts via events
-	for use.
-	
-	Remember -- A "Caster" represents an entire gun (or whatever is launching your projectiles), *NOT* the individual bullets.
-	Make the caster once, then use the caster to fire your bullets. Do not make a caster for each bullet.
+  Written by Eti the Spirit (18406183)
+  
+    The latest patch notes can be located here (and do note, the version at the top of this script might be outdated. I have a thing for forgetting to change it):
+    > https://etithespirit.github.io/FastCastAPIDocs/changelog
+    
+    *** If anything is broken, please don't hesitate to message me! ***
+    
+    YOU CAN FIND IMPORTANT USAGE INFORMATION HERE: https://etithespirit.github.io/FastCastAPIDocs
+    YOU CAN FIND IMPORTANT USAGE INFORMATION HERE: https://etithespirit.github.io/FastCastAPIDocs
+    YOU CAN FIND IMPORTANT USAGE INFORMATION HERE: https://etithespirit.github.io/FastCastAPIDocs
+    
+    YOU SHOULD ONLY CREATE ONE CASTER PER GUN.
+    YOU SHOULD >>>NEVER<<< CREATE A NEW CASTER EVERY TIME THE GUN NEEDS TO BE FIRED.
+    
+    A caster (created with FastCast.new()) represents a "gun".
+    When you consider a gun, you think of stats like accuracy, bullet speed, etc. This is the info a caster stores. 
+  
+  --
+  
+  This is a library used to create hitscan-based guns that simulate projectile physics.
+  
+  This means:
+    - You don't have to worry about bullet lag / jittering
+    - You don't have to worry about keeping bullets at a low speed due to physics being finnicky between clients
+    - You don't have to worry about misfires in bullet's Touched event (e.g. where it may going so fast that it doesn't register)
+    
+  Hitscan-based guns are commonly seen in the form of laser beams, among other things. Hitscan simply raycasts out to a target
+  and says whether it hit or not.
+  
+  Unfortunately, while reliable in terms of saying if something got hit or not, this method alone cannot be used if you wish
+  to implement bullet travel time into a weapon. As a result of that, I made this library - an excellent remedy to this dilemma.
+  
+  FastCast is intended to be require()'d once in a script, as you can create as many casters as you need with FastCast.new()
+  This is generally handy since you can store settings and information in these casters, and even send them out to other scripts via events
+  for use.
+  
+  Remember -- A "Caster" represents an entire gun (or whatever is launching your projectiles), *NOT* the individual bullets.
+  Make the caster once, then use the caster to fire your bullets. Do not make a caster for each bullet.
 --]]
 
 -----------------------------------------------------------
@@ -54,8 +54,8 @@ FastCast.__type = "FastCast" -- For compatibility with TypeMarshaller
 
 -- Extra stuff
 FastCast.HighFidelityBehavior = {
-	Default = 1,
-	Always = 3
+  Default = 1,
+  Always = 3
 }
 
 -----------------------------------------------------------
@@ -82,8 +82,6 @@ local ERR_OBJECT_DISPOSED = "This Caster has been disposed. It can no longer be 
 -----------------------------------------------------------
 
 -- This will inject all types into this context.
--- YES, THIS MEANS YOU IGNORE MISSING TYPE ERRORS. Remember: Type checking is still in beta!
--- * As of release
 local TypeDefs = require(script.TypeDefinitions)
 type CanPierceFunction = TypeDefs.CanPierceFunction
 type GenericTable = TypeDefs.GenericTable
@@ -107,41 +105,41 @@ ActiveCastStatic.SetStaticFastCastReference(FastCast)
 
 -- Constructor.
 function FastCast.new()
-	return setmetatable({
-		LengthChanged = Signal.new("LengthChanged"),
-		RayHit = Signal.new("RayHit"),
-		RayPierced = Signal.new("RayPierced"),
-		CastTerminating = Signal.new("CastTerminating"),
-		WorldRoot = workspace
-	}, FastCast)
+  return setmetatable({
+    LengthChanged = Signal.new("LengthChanged"),
+    RayHit = Signal.new("RayHit"),
+    RayPierced = Signal.new("RayPierced"),
+    CastTerminating = Signal.new("CastTerminating"),
+    WorldRoot = workspace
+  }, FastCast)
 end
 
 -- Create a new ray info object.
 -- This is just a utility alias with some extra type checking.
 function FastCast.newBehavior(): FastCastBehavior
-	-- raycastParams, maxDistance, acceleration, canPierceFunction, cosmeticBulletTemplate, cosmeticBulletContainer, autoIgnoreBulletContainer
-	return {
-		RaycastParams = nil,
-		Acceleration = Vector3.new(),
-		MaxDistance = 1000,
-		CanPierceFunction = nil,
-		HighFidelityBehavior = FastCast.HighFidelityBehavior.Default,
-		HighFidelitySegmentSize = 0.5,
-		CosmeticBulletTemplate = nil,
-		CosmeticBulletProvider = nil,
-		CosmeticBulletContainer = nil,
-		AutoIgnoreContainer = true
-	}
+  -- raycastParams, maxDistance, acceleration, canPierceFunction, cosmeticBulletTemplate, cosmeticBulletContainer, autoIgnoreBulletContainer
+  return {
+    RaycastParams = nil,
+    Acceleration = Vector3.new(),
+    MaxDistance = 1000,
+    CanPierceFunction = nil,
+    HighFidelityBehavior = FastCast.HighFidelityBehavior.Default,
+    HighFidelitySegmentSize = 0.5,
+    CosmeticBulletTemplate = nil,
+    CosmeticBulletProvider = nil,
+    CosmeticBulletContainer = nil,
+    AutoIgnoreContainer = true
+  }
 end
 
 local DEFAULT_DATA_PACKET = FastCast.newBehavior()
 function FastCast:Fire(origin: Vector3, direction: Vector3, velocity: Vector3 | number, castDataPacket: FastCastBehavior?): ActiveCast
-	if castDataPacket == nil then castDataPacket = DEFAULT_DATA_PACKET end
-	
-	local cast = ActiveCastStatic.new(self, origin, direction, velocity, castDataPacket)
-	cast.RayInfo.WorldRoot = self.WorldRoot
-	return cast
+  if castDataPacket == nil then castDataPacket = DEFAULT_DATA_PACKET end
+  
+  local cast = ActiveCastStatic.new(self, origin, direction, velocity, castDataPacket)
+  cast.RayInfo.WorldRoot = self.WorldRoot
+  return cast
 end
 
 -- Export
-return
+return FastCast
